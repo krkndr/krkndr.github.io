6ebf9604bf6710a8fae55b4882132a639ff401e7
krkndr <125579637+krkndr@users.noreply.github.com>
2023-12-14 16:00:58 +0200

    Update save_events_1.php

--- lab7/save_events_1.php
+++ lab7/save_events_1.php
@@ -1,7 +1,5 @@
 <?php
 $data = json_decode(file_get_contents('php://input'), true);
-//$serverTime = time(); 
-//$localTime = $data['timestamp'] + ($serverTime - $data['serverTime']);
 $localTime = strtotime($data['timestamp']) + 3600;
 $logEntry = sprintf("[%s] Event #%d: %s\n", date('Y-m-d H:i:s', $localTime), $data['eventNumber'], $data['message']);
 file_put_contents('events.txt', $logEntry, FILE_APPEND | LOCK_EX);
