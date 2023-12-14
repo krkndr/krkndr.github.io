128f6679f6805d68bbd488e7a1fb745ade66754e
krkndr <125579637+krkndr@users.noreply.github.com>
2023-12-14 16:00:26 +0200

    Update clear.php

--- lab7/clear.php
+++ lab7/clear.php
@@ -1,13 +1,7 @@
 <?php
-// Шлях до файлу
 $filePath1 = 'events.txt';
-
-// Очищення файлу
 file_put_contents($filePath1, '');
 $filePath2 = 'events2.txt';
-
-// Очищення файлу
 file_put_contents($filePath2, '');
-// Відповідь на клієнта
 http_response_code(200);
 ?>
